442fa893f871ef909fd019f68d22ec5cff8cfc69
MARIA EDUARDA LOPES DE ANDRADE <maria.elandrade2@senacsp.edu.br>
2026-09-09 21:47:00 -0300

    Atualizado

---
+++ js/js.js
@@ -0,0 +1,45 @@
+let saida, i;
+
+function contar(){
+    saida = ""
+    for (i=0;i<=10;i++){
+        saida = saida + i + "<br>";
+    }
+
+    document.getElementById("resultado").innerHTML = saida;
+}
+
+let saida1, i1;
+
+function contar2(){
+    saida1 = "";
+    for (i1=10;i1>=0;i1--){
+        saida1 = saida1 + i1 + "<br>";
+    }
+
+    document.getElementById("resultado2").innerHTML = saida1;
+}
+
+let inicio, a, saida3;
+
+function ContarAteCem() {
+    inicio = Number(document.getElementById("Inicio").value);
+    saida3  = "";
+    for (a = inicio;a<=100;a++) {
+        saida3 = saida3 + a + "<br>"
+    }
+
+    document.getElementById("ateCem").innerHTML = saida3
+}
+
+let tabuada1, n, result;
+
+function Tabuada() {
+    tabuada1 = Number(document.getElementById("iniciodaTab").value);
+    result = "";
+    for (n = 0;n<=10;n++) {
+        result = result + tabuada1 + "x" + n + "=" + (tabuada1 *  n) + "<br>"
+    }
+    document.getElementById("tabuada").innerHTML = result;
+}
+
